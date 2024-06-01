769c4a167bf2b3bcf2444f3b8a38db4f07d8a21a
Sonali Priya <117668067+sonalipriyaa@users.noreply.github.com>
2024-06-01 13:41:00 +0530

    Create Reverse_linked_list.cpp

---
+++ Reverse_linked_list.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        Node* prev=nullptr;
+        Node* curr=head;
+        Node* next=curr->next;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    
+};
